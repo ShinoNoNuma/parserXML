d40269028366fab2eeaa00692dbd39d5339de7aa
samyyounsi <samy.webdev@gmail.com>
2015-08-04 05:33:52 +0200

    Add database.php

---
+++ app/Config/database.php
@@ -0,0 +1,26 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'parsexml',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'parsexml',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+}
